cf8266cf2d77850fbd1a53469e235c0d72830568
EANimesha <nimeshadilini999@gmail.com>
2019-11-15 16:47:18 +0530

    add routes for register and login for a viewer

---
+++ routes/Viewers.js
@@ -0,0 +1,75 @@
+const express= require('express');
+const viewers=express.Router();
+
+const cors=require("cors");
+// const bcrypt=require("bcrypt");
+const bcrypt=require('bcrypt-nodejs')
+
+const Viewer=require("../models/Viewer");
+viewers.use(cors())
+
+//get viewer data
+viewers.get('/',(req,res)=>{
+    Viewer.find(function(err,viewers){
+        res.json(viewers);
+    })
+})
+
+//add a viewer
+viewers.post('/register',(req,res)=>{
+    const today=new Date();
+    const userData={
+        first_name:req.body.first_name,
+        last_name:req.body.last_name,
+        email:req.body.email,
+        password:req.body.password,
+        created:today
+    }
+    Viewer.findOne({
+        email:req.body.email
+    })
+    .then(user=>{
+        if(!user){
+            const hash =bcrypt.hashSync(userData.password);
+            // bcrypt.hash(req.body.password,10,(err,hash)=>{
+            userData.password=hash;
+            Viewer.create(userData)
+            .then(user=>{
+                   res.json({status:user.email+' Registered'})
+             })
+            .catch(err=>{
+                res.send('error: '+err);
+           })
+            // })
+        }else{
+            res.json({error:'User already exists'})
+        }
+    })
+    .catch(err=>{
+        res.send('error : '+err)
+    })
+})
+
+
+//viewer login
+viewers.post('/login',(req,res)=>{
+    Viewer.findOne({
+        email:req.body.email
+    })
+    .then(user=>{
+        if(user){
+            if(bcrypt.compareSync(req.body.password,user.password)){
+                return res.json(user);
+            }else{
+                res.status(400).json('wrong credentials')
+            }
+        }else{
+            res.json({error:'User does not exist'})
+        }
+    })
+    .catch(err=>{
+        res.send('error '+err)
+    })
+})
+
+module.exports=viewers;
