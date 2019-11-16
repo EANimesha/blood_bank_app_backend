f03ab791baaa0a7e1393b94fe90da122f884b22f
EANimesha <nimeshadilini999@gmail.com>
2019-11-17 04:46:43 +0530

    add some more fields for user model

--- models/Viewer.js
+++ models/Viewer.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
 const Schema=mongoose.Schema
+var bcrypt = require('bcrypt');
 
 const UserSchema=new Schema({
     first_name:{
@@ -16,10 +17,27 @@ const UserSchema=new Schema({
         type:String,
         required:true
     },
+    donar:{
+        type:Boolean,
+        require:true
+    },
     date:{
         type:Date,
         default:Date.now
+    },
+    blood_type:{
+        type:String,
+    },
+    age:{
+        type:String
+    },
+    weight:{
+        type:String
+    },
+    height:{
+        type:String,
     }
 });
+
 
 module.exports=Viewer=mongoose.model('viewers',UserSchema);
