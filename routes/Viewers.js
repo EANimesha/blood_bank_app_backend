9ec0ab6282dbb010bbc14f585a8dc5dccd58c6dc
EANimesha <nimeshadilini999@gmail.com>
2019-11-30 22:31:16 +0530

    user authentiication with jwt token passing

--- routes/Viewers.js
+++ routes/Viewers.js
@@ -1,4 +1,5 @@
 const express= require('express');
+const jwt=require('jsonwebtoken');
 const viewers=express.Router();
 
 const cors=require("cors");
@@ -60,20 +61,30 @@ viewers.post('/login',(req,res)=>{
     Viewer.findOne({
         email:req.body.email
     })
+    .exec()
     .then(user=>{
         if(user){
             if(bcrypt.compareSync(req.body.password,user.password)){
-                return res.json(user);
+                const token=jwt.sign({user},"secret",{expiresIn:"1h"},(err, token) => {
+                    if(err) { console.log(err) }    
+                    res.json(token);
+                });
+                // // return res.json(user);
+                // return res.status(200).json({
+                //     message:"Auth successful",
+                //     token:token
+                // })
             }else{
                 res.status(400).json('wrong credentials')
             }
         }else{
-            res.json({error:'User does not exist'})
+            res.json({error:'User does not exist'}) 
         }
     })
     .catch(err=>{
-        res.send('error '+err)
+        res.send('error '+err) 
     })
+     
 })
 
 // viewers.post('/login',(req,res)=>{
@@ -108,15 +119,66 @@ viewers.get('/donars',(req,res)=>{
 })
 
 //get a certain user by id
-viewers.get('/:id',(req,res)=>{
-    const {id}=req.params;
-    Viewer.find({_id:id})
-    .then(user=>res.json(user))
-    .catch(err=>{
-        res.send('error '+err)
+viewers.get('/user',verifyToken,(req,res)=>{
+    jwt.verify(req.body.token,'secret',(err,authData)=>{
+        if(err){
+            res.sendStatus(403);
+        }else{
+            var user=authData["user"]
+            res.status(200).json(
+                // {
+                // message:'verified user',
+                authData
+            // }
+            );
+        }
     })
+    // const {id}=req.params;
+    // Viewer.find({_id:id})
+    // .then(user=>res.json(user))
+    // .catch(err=>{
+    //     res.send('error '+err)
+    // })
 })
 
+async function verifyToken(req,res,next){
+    const header = req.headers['authorization'];
+    
+    if(typeof header !== 'undefined') {
+        const bearer = header.split(' ');
+        const token = bearer[0];
+        req.body.token = token;
+        next();
+    } else {
+        //If header is undefined return Forbidden (403)
+        res.sendStatus(403)
+    }
 
+    // console.log(req.header.authorization);
+    // if(typeof req.headers.authorization !=="undefined"){
+    //     let token=req.headers.authorization.split(" ")[1];
+    //     jwt.verify(token,'secret',(err,decoded)=>{
+    //         if(err){
+    //             res.json("not authorized")
+    //         }
+    //         console.log(decoded);
+    //         return next();
+
+    //     })
+    // }else{
+    //     res.json("not Authorized")
+    // }
+
+
+    // const bearerHeader=req.headers['authorization'];
+    // if(typeof bearerHeader!=='undefined'){
+    //     const bearer= bearerHeader.split(' ');
+    //     const bearerToken=bearer[1];
+    //     req.token=bearerToken;
+    //     next();
+    // }else{
+    //     res.sendStatus(403)
+    // }
+}
 
 module.exports=viewers;
